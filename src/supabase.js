ddd86a13ed42869eba40a178607e6d1b7410aef4
piermartinifernando-blip <piermartinifernando@gmail.com>
2026-03-23 10:20:05 -0300

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const URL = 'https://ozlmgitabjmtxjuyhylh.supabase.co'
+const KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96bG1naXRhYmptdHhqdXloeWxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNTUyNjEsImV4cCI6MjA4OTgzMTI2MX0.ypCXz0qit5hnNBEkt_iqXccsJDYnf8thraL8K4OtaDU'
+
+export const supabase = createClient(URL, KEY)
